bfa44346875e2ea44151f34f5b649e92b72f0339
tejasmethawade <87464870+tejasmethawade@users.noreply.github.com>
2023-09-11 08:08:20 +0530

    thirdcommit.jsx

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -172,7 +172,7 @@ const Footer = () => {
             })}
           </div>
 
-          <div className="text-center">Made with ❤️ CodeHelp © 2023 Studynotion</div>
+          <div className="text-center">Copyrights @Tejas Methawade </div>
         </div>
       </div>
     </div>
